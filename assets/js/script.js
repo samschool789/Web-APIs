7bcef9f9404f1da7de1e3b10ffb7d465ee5840f4
samschool789@gmail.com <samschool789@gmail.com>
2022-04-08 21:33:10 -0500

    html and css

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+//add my variables i will need 
+
+var startButton = document.getElementById('start-btn')
+
+
+
+
+// add an event listener for the start button
+
+startButton.addEventListener('click', startButton) 
+    console.log('yay')
+    startButton.classList.list.add('hide')
+
+
+    //add start game function
+    function startGame ()
+
+//start a timer when i click that button
+
+
+//have a question pop up when the timer starts
+
+
+
+//when i answer the question another one pops up 
+
+
+//make it to where if i answer the question incorrectly it subracts time from the clock 
+
+
+
+//when timer reaches 0 or all questions answered END THE GAME
+
+
+
+//use local storage to save scores
+
+
+//have my 5 questions..... 1.  What is 
+
